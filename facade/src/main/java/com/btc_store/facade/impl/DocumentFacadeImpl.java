a78380b98b427a1193b122ca1d9bd379c4b7aac8
Mahmut Çöpoğlu <mahmut.copoglu@btc-ag.com.tr>
2026-02-16 21:20:55 +0300

    main

--- facade/src/main/java/com/btc_store/facade/impl/DocumentFacadeImpl.java
+++ facade/src/main/java/com/btc_store/facade/impl/DocumentFacadeImpl.java
@@ -52,7 +52,6 @@ public class DocumentFacadeImpl implements DocumentFacade {
     public DocumentData saveDocument(DocumentData documentData, List<MultipartFile> mediaFiles) {
         DocumentModel documentModel;
         var siteModel = siteService.getCurrentSite();
-        List<MediaModel> existingMediaFiles = new ArrayList<>();
 
         if (documentData.isNew()) {
             documentModel = modelMapper.map(documentData, DocumentModel.class);
@@ -60,10 +59,10 @@ public class DocumentFacadeImpl implements DocumentFacade {
             documentModel.setSite(siteModel);
         } else {
             documentModel = searchService.searchByCodeAndSite(DocumentModel.class, documentData.getCode(), siteModel);
-            existingMediaFiles = new ArrayList<>(documentModel.getMedias());
             modelMapper.map(documentData, documentModel);
         }
 
+        // Ürünleri bağla
         List<ProductModel> products = new ArrayList<>();
         if (CollectionUtils.isNotEmpty(documentData.getProducts())) {
             for (var product : documentData.getProducts()) {
@@ -72,7 +71,32 @@ public class DocumentFacadeImpl implements DocumentFacade {
         }
         documentModel.setProducts(products);
 
-        handleDocumentMediaFiles(documentData, documentModel, mediaFiles, existingMediaFiles, siteModel);
+        // Media dosyalarını yükle
+        if (Objects.nonNull(mediaFiles) && !mediaFiles.isEmpty()) {
+            try {
+                var cmsCategoryModel = cmsCategoryService.getCmsCategoryByCode(MediaCategory.PRODUCT.getValue(), siteModel);
+                List<MediaModel> uploadedMediaFiles = new ArrayList<>();
+                
+                for (MultipartFile mediaFile : mediaFiles) {
+                    if (!mediaFile.isEmpty()) {
+                        var mediaModel = mediaService.storage(mediaFile, false, cmsCategoryModel, siteModel);
+                        uploadedMediaFiles.add(mediaModel);
+                    }
+                }
+                
+                // Mevcut dosyalara yeni dosyaları ekle
+                if (documentModel.getMedias() == null) {
+                    documentModel.setMedias(uploadedMediaFiles);
+                } else {
+                    documentModel.getMedias().addAll(uploadedMediaFiles);
+                }
+                
+                log.info("Uploaded {} new media files for document", uploadedMediaFiles.size());
+            } catch (Exception e) {
+                log.error("Error uploading document media files: {}", e.getMessage());
+                throw new RuntimeException("Error uploading document media files: " + e.getMessage());
+            }
+        }
 
         var savedModel = modelService.save(documentModel);
         return modelMapper.map(savedModel, DocumentData.class);
@@ -85,74 +109,5 @@ public class DocumentFacadeImpl implements DocumentFacade {
         documentModel.setDeleted(true);
         documentModel.setActive(false);
         modelService.save(documentModel);
-    }
-
-    private void handleDocumentMediaFiles(DocumentData documentData, DocumentModel documentModel,
-                                          List<MultipartFile> mediaFiles, List<MediaModel> existingMediaFiles,
-                                          SiteModel siteModel) {
-        if (Objects.nonNull(mediaFiles) && !mediaFiles.isEmpty()) {
-            uploadAndSetNewMediaFiles(documentData, documentModel, mediaFiles, existingMediaFiles, siteModel);
-        } else {
-            updateExistingMediaFiles(documentData, documentModel, existingMediaFiles);
-        }
-    }
-
-    private void uploadAndSetNewMediaFiles(DocumentData documentData, DocumentModel documentModel,
-                                           List<MultipartFile> mediaFiles, List<MediaModel> existingMediaFiles,
-                                           SiteModel siteModel) {
-        try {
-            var cmsCategoryModel = cmsCategoryService.getCmsCategoryByCode(MediaCategory.PRODUCT.getValue(), siteModel);
-            List<MediaModel> finalMediaFiles = new ArrayList<>();
-
-            addExistingMediaFilesInOrder(documentData, existingMediaFiles, finalMediaFiles);
-            uploadNewMediaFiles(mediaFiles, cmsCategoryModel, siteModel, finalMediaFiles);
-
-            documentModel.setMedias(finalMediaFiles);
-            log.info("Total {} media files processed", finalMediaFiles.size());
-        } catch (Exception e) {
-            log.error("Error uploading document media files: {}", e.getMessage());
-            throw new RuntimeException("Error uploading document media files: " + e.getMessage());
-        }
-    }
-
-    private void addExistingMediaFilesInOrder(DocumentData documentData, List<MediaModel> existingMediaFiles,
-                                              List<MediaModel> finalMediaFiles) {
-        if (CollectionUtils.isNotEmpty(documentData.getMediaFileCodesInOrder())) {
-            for (String mediaCode : documentData.getMediaFileCodesInOrder()) {
-                existingMediaFiles.stream()
-                        .filter(media -> media.getCode().equals(mediaCode))
-                        .findFirst()
-                        .ifPresent(finalMediaFiles::add);
-            }
-            log.info("{} existing media files preserved", finalMediaFiles.size());
-        }
-    }
-
-    private void uploadNewMediaFiles(List<MultipartFile> mediaFiles, CmsCategoryModel cmsCategoryModel,
-                                     SiteModel siteModel, List<MediaModel> finalMediaFiles) {
-        for (MultipartFile mediaFile : mediaFiles) {
-            if (!mediaFile.isEmpty()) {
-                var mediaModel = mediaService.storage(mediaFile, false, cmsCategoryModel, siteModel);
-                finalMediaFiles.add(mediaModel);
-            }
-        }
-        log.info("{} new media files uploaded", mediaFiles.size());
-    }
-
-    private void updateExistingMediaFiles(DocumentData documentData, DocumentModel documentModel,
-                                          List<MediaModel> existingMediaFiles) {
-        if (Objects.nonNull(documentData.getMediaFileCodesInOrder())) {
-            List<MediaModel> orderedMediaFiles = new ArrayList<>();
-            for (String mediaCode : documentData.getMediaFileCodesInOrder()) {
-                existingMediaFiles.stream()
-                        .filter(media -> media.getCode().equals(mediaCode))
-                        .findFirst()
-                        .ifPresent(orderedMediaFiles::add);
-            }
-            documentModel.setMediaFiles(orderedMediaFiles);
-            log.info("Media files order updated. New: {}, Original: {}", orderedMediaFiles.size(), existingMediaFiles.size());
-        } else {
-            documentModel.setMediaFiles(existingMediaFiles);
-        }
     }
 }
