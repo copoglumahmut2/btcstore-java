d90ef5cfa8f4b06115d0f71cfa50b1dbb55e1c06
Mahmut Çöpoğlu <mahmut.copoglu@btc-ag.com.tr>
2026-02-16 17:07:53 +0300

    main

--- rebuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Cleaning and rebuilding backend...
-cd /d c:\Users\mcopoglu\Desktop\btcstore
-call mvn clean
-call mvn install -DskipTests
-echo Done! Now restart your backend application.
-pause
